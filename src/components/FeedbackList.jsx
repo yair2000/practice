4030a680720518492c593fe1557e9a6758bcd21d
yair2000 <32851854+yair2000@users.noreply.github.com>
2022-02-14 21:02:16 +0200

    Add files via upload

--- src/components/FeedbackList.jsx
+++ src/components/FeedbackList.jsx
@@ -2,10 +2,15 @@ import { useContext } from "react";
 import { motion, AnimatePresence } from "framer-motion"
 import FeedbackItem from "./FeedbackItem";
 import FeedbackContext from "../context/FeedbackContext";
+import Spinner from "./shared/Spinner";
 
 function FeedbackList(){
-  const {feedback} = useContext(FeedbackContext)
-  return(
+  const {feedback, loading} = useContext(FeedbackContext)
+
+  if(!loading && (!feedback || feedback.length === 0)){
+    return <p>No feedbacks</p>
+  }
+  return loading ? <Spinner/> : (
     <div className="feedback-list">
       <AnimatePresence>
         {feedback.map((item) =>(
--- src/context/FeedbackContext.jsx
+++ src/context/FeedbackContext.jsx
@@ -1,35 +1,46 @@
-import {createContext, useState} from "react"
-import {v4 as uuidv4} from "uuid"
+import {createContext, useState, useEffect} from "react"
 const FeedbackContext = createContext();
 
 export const FeedbackProvider = ({children}) =>{
-  const [feedback, setFeedback] = useState([
-    {
-      id: 1,
-      text: "Context text 1",
-      rating: 7
-    },
-    {
-      id: 2,
-      text: "Context text 2",
-      rating: 6
-    }
-  ]);
+  const [feedback, setFeedback] = useState([]);
+  const [loading, setLoading] = useState(true);
   const [editFB, setEditFB] = useState({
     item: {},
     edit: false
   });
 
+  useEffect(() =>{
+    fetchFeedback();
+  }, []);
+
+  
+  const fetchFeedback = async () =>{
+    const response = await fetch("http://localhost:5000/feedback?_sort=rating")
+    const data = await response.json();
+
+    setFeedback(data);
+    setLoading(false);
+  }
+
   // Create/Read
-  const addFeedback = (newFeedback) =>{
-    newFeedback.id = uuidv4();
-    setFeedback([newFeedback, ...feedback]);
+  const addFeedback = async (newFeedback) => {
+    const response = await fetch('http://localhost:5000/feedback', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify(newFeedback),
+    });
+
+    const data = await response.json();
+    setFeedback([data, ...feedback]);
   }
 
   // Delete
-  const deleteFeedback = (id) =>{
+  const deleteFeedback = async (id) =>{
     if(window.confirm("Are You Sure?")){
-      setFeedback(feedback.filter((item) => item.id !==id))
+      await fetch(`/feedback/${id}`, {method: 'DELETE'})
+      setFeedback(feedback.filter((item) => item.id !==id));
     }
   }
 
@@ -42,14 +53,24 @@ export const FeedbackProvider = ({children}) =>{
   }
 
   // Update
-  const updateFeedback = (id, updatedItem) =>{
-    setEditFB(feedback.map((item) => item.id === id ? {...item, ...updatedItem} : item))
+  const updateFeedback = async (id, updatedItem) =>{
+    const response = await fetch(`http://localhost:5000/feedback/${id}`,{
+      method: 'PUT',
+      headers: {
+        'Content-Type': 'application/json'
+      },
+      body: JSON.stringify(updatedItem)
+    });
+    const data = await response.json();
+
+    setFeedback(feedback.map((item) => item.id === id ? {...item, ...data} : item));
   }
 
   return(
     <FeedbackContext.Provider value={{
       feedback,
       editFB,// From the useState hook
+      loading,
       deleteFeedback,
       addFeedback,
       editFeedback, // The name of the function that edits our review text
